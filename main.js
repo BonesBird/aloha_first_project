17cf77c737e057e8d3c790686ad7c9d7ac3ab9b1
ronald <r.gaskin@live.ca>
2019-04-18 16:59:20 -0700

    updated

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function (carousel) {
 
     console.log('works');
-
+    const flkty = new Flickity('.main-carousel', {
+        cellAlign: 'left',
+        contain: 'true',
+        freeScroll: 'true',
+        wrapAround: 'true',
+    }
+    )
 });
 
-
-// const mostLoved = [
-//     'Go to space',
-//     'smoke a jay',
-//     'listen Pink Floyd to the wall',
-//     'pet my cat',
-//   ];
-//   // Write JS to append each array item to the DOM here
-//   // 1: get the bucketlist element
-//   const loveShopContainer = document.getElementById('mostLoved');
-//   // 2: create a new li element e.g. document.createElement();
-//   const item1 = document.createElement('li');
-//   const item2 = document.createElement('li');
-//   const item3 = document.createElement('li');
-//   const item4 = document.createElement('li');
-  
-//   item1.innerText = bucketList[0];
-//   item2.innerText = bucketList[1];
-//   item3.innerText = bucketList[2];
-//   item4.innerText = bucketList[3];
-  
-//   loveShopContainer.append(item1);
-//   loveShopContainer.append(item2);
-//   loveShopContainer.append(item3);
-//   loveShopContainer.append(item4);
-  
-//   console.log('mostLoved');
