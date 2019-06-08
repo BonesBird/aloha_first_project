4bf3a6a90293cc33472664827dab849a1ee41e12
ronald <r.gaskin@live.ca>
2019-06-07 18:30:31 -0700

    working-on-cleaning-up-code-css-next

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ document.addEventListener("DOMContentLoaded", function (carousel) {
     }
     )
 });
+// main accessibility code
 
+$(function() {
+    $("a[href^='#']").not("a[href='#']").click(function() {
+       $("#"+$(this).attr("href").slice(1)+"").focus();
+    });
+ });
