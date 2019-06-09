8a64eed1273d75d98e39189663904be5d31c794e
ronald <r.gaskin@live.ca>
2019-06-09 15:56:18 -0700

    working-on-top-site-area

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", function (carousel) {
-
-    console.log('works');
-    const flkty = new Flickity('.main-carousel', {
-        cellAlign: 'left',
-        contain: 'true',
-        freeScroll: 'true',
-        wrapAround: 'true',
-    }
-    )
+document.addEventListener("DOMContentLoaded", function(carousel) {
+  console.log("works");
+  const flkty = new Flickity(".main-carousel", {
+    cellAlign: "left",
+    contain: "true",
+    freeScroll: "true",
+    wrapAround: "true"
+  });
 });
 // main accessibility code
 
 $(function() {
-    $("a[href^='#']").not("a[href='#']").click(function() {
-       $("#"+$(this).attr("href").slice(1)+"").focus();
+  $("a[href^='#']")
+    .not("a[href='#']")
+    .click(function() {
+      $(
+        "#" +
+          $(this)
+            .attr("href")
+            .slice(1) +
+          ""
+      ).focus();
     });
- });
+});
